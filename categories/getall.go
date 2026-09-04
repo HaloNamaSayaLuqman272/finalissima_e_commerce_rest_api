50a1af1c8e125544bb5db7bddb39a645751c5799
HaloNamaSayaLuqman272 <mluqman272@gmail.com>
2026-09-04 21:45:30 +0700

    initial commit

--- categories/getall.go
+++ categories/getall.go
@@ -2,15 +2,27 @@ package categories
 
 import (
 	"context"
+	"finalissima_e_commerce_rest_api/package/utils"
 
 	"gorm.io/gorm"
 )
 
-type GetAllRequest struct {
+type getall struct {
+	// kita membuat type struct bernama "getall" yg rencana nya akan digunakan
+	// untuk menampilkan semua data kategori yg ada
 	repository *gorm.DB
 	create
+	// dalam type struct "getall" kita membutuhkan "repository" yg terhubung dengan *gorm.DB
+	// dan juga kita ingin type struct "getall" mendapatkan akses koneksi ke "create"
 }
 
-func (g getAll) GetAllCategories(ctx context.Context) ([]Category, error) {
+func (g getall) GetAllCategories(ctx context.Context, pagination utils.Pagination) (utils.Pagination, error) {
+	// kita mendeklarasikan sebuah fungsi dengan nama "GetAllCategories"
+	// memiliki variabel penerima "g" bertipe data "getall"
+	// terdapat parameter input "ctx" tipe data dari "context.Context" untuk menjaga lifcycle
+	// dan "pagination" tipe data dari "utils.Pagination" yg akan menampilkan
+	// jumlah halaman dan per halaman akan menampilkan maksimal berapa baris data
+	categories := []Category{}
+	//
 
 }
