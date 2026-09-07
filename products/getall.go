d4f68f4913a56865e98ae16c5b1c48f83b073ba7
HaloNamaSayaLuqman272 <mluqman272@gmail.com>
2026-09-07 13:27:08 +0700

    initial commit

--- products/getall.go
+++ products/getall.go
@@ -5,6 +5,7 @@ import (
 	"finalissima_e_commerce_rest_api/package/utils"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type getall struct {
@@ -13,4 +14,23 @@ type getall struct {
 
 func (g getall) GetAllProduct(ctx context.Context, pagination utils.Pagination) (utils.Pagination, error) {
 	products := []Product{}
+
+	if err := g.repository.WithContext(ctx).Scopes(utils.Paginate(&products, &pagination, g.repository)).Preload(clause.Associations).Find(&products).Error; err != nil {
+		// "g.repository.WithContext(ctx)" untuk menghubungkan ke database dan
+		// ".WithContext(ctx)" menjaga proses lifecycle aplikasi Go, jika
+		// dari user melakukan cancel atau sambungan database ada masalah
+		// maka akan proses ini akan dihentikan
+		// ".Scopes(...)" kita ingin menggunakan "Pagination" dari package
+		// "utils" untuk menampilkan semua "products" yg ada di database
+		// dan tampilkan halaman per halaman
+		// ".Preload(clause.Associations)" adalah cara memuat semua data relasi/
+		// asosiasi sekaligus secara otomatis
+		return utils.Pagination{}, err
+		// jika terdapat masalah atau error saat proses pengambilan maka kembalikan
+		// "Pagination" kosongan dan error
+	}
+
+	pagination.Rows = products
+
+	return pagination, nil
 }
