0e1ca02b9fdfd7dfa5076b5c6988f4f463c125a4
HaloNamaSayaLuqman272 <mluqman272@gmail.com>
2026-09-07 14:02:45 +0700

    initial commit

--- products/getbycategory.go
+++ products/getbycategory.go
@@ -1,7 +1,25 @@
 package products
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"finalissima_e_commerce_rest_api/package/utils"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
 
 type getbycategory struct {
 	repository *gorm.DB
+}
+
+func (g getbycategory) GetProductByCategory(ctx context.Context, pagination utils.Pagination, categoryId uint) (utils.Pagination, error) {
+	products := []Product{}
+
+	if err := g.repository.WithContext(ctx).Scopes(utils.PaginateByProductCategory(&products, &pagination, categoryId, g.repository)).Preload(clause.Associations).Find(&products).Error; err != nil {
+		return utils.Pagination{}, err
+	}
+
+	pagination.Rows = products
+
+	return pagination, nil
 }
