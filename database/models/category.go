286cc4b2e4dbc221eb5a247e256351ddde8c788e
HaloNamaSayaLuqman272 <mluqman272@gmail.com>
2026-09-15 20:30:27 +0700

    initial commit

--- database/models/category.go
+++ database/models/category.go
@@ -10,7 +10,7 @@ import (
 // kita membuat type struct "Category"
 type Category struct {
 	ID           uint            `json:"id" gorm:"primaryKey;autoIncrement"`
-	NameCategory string          `json:"name_category" gorm:"unique"`
+	NameCategory string          `json:"name_category" gorm:"uniqueIndex:idx_name_category_active,where:deleted_at IS NULL"`
 	Description  string          `json:"description"`
 	CreatedAt    time.Time       `json:"created_at"`
 	UpdatedAt    time.Time       `json:"updated_at"`
